45fa4da2d4380bd24e80323ae684174dd7ae1cef
PrabhatA77 <rathorevasu498@gmail.com>
2025-11-05 17:55:01 +0530

    commented  some lines in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,8 +13,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 app.use(cors({origin:"http://localhost:5173",credentials:true}));
 app.use(express.json());
@@ -22,15 +22,15 @@ app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  // Serve frontend static files
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   // Serve frontend static files
+//   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
   
-  // Serve index.html for all remaining routes (React router support)
-  app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
-  });
-}
+//   // Serve index.html for all remaining routes (React router support)
+//   app.use((req, res) => {
+//     res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+//   });
+// }
 
 app.get('/',(req,res)=>{
     res.send('Api working fine noob');
